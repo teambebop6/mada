bc58dbd13b50b7d681c5360501df69d8eea37efe
Henry Huang <h1886@outlook.com>
2023-11-01 22:57:31 +0800

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          MADAGASKARHAUS
+        </p>
+        <a
+          className="App-link"
+        >
+          das Informations- und Reisezentrum für Madagaskar
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
